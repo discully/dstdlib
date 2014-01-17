3a06534a807f6c2624b43550f8b9bc500bd1e3bb
Daniel Scully <daniel.i.scully@gmail.com>
2014-01-17 10:53:06 +0000

    tidied up the test, ready for adding a lot more

--- test/test_vector.cxx
+++ test/test_vector.cxx
@@ -1,19 +1,29 @@
-#include "vector.hxx"
+#include <iostream>
+#include "../src/vector.hxx"
+
+template <class T>
+void printVector(dstd::vector<T>& v)
+{
+	typename dstd::vector<T>::iterator it = v.begin();
+	const typename dstd::vector<T>::iterator end = v.end();
+	for( ; it != end; ++it)
+	{
+		std::cout << *it << ", ";
+	}
+	std::cout << std::endl;
+}
 
 int main()
 {
-	dstd::vector<int> v_int();
+	dstd::vector<int> v_int;
 	for(int i = 0; i != 6; ++i)
 	{
 		v_int.push_back(i);
 	}
 	
-	dstd::vector<int>::iterator it = v_int.begin();
-	const dstd::vector<int>::iterator end = v_int.end();
-	for( ; it != end; ++it)
-	{
-		std::cout << *it << std::endl;
-	}
+	printVector<int>( v_int );
+	
+	
 	
 	return 0;
 }
