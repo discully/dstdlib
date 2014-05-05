eac1bf67353e4fe66cedf1be02523d2b2a4e8214
Daniel Scully <daniel.i.scully@gmail.com>
2014-05-05 08:16:25 +0100

    moved implementation back into header, as was done for vector

--- src/allocator.hxx
+++ src/allocator.hxx
@@ -1,6 +1,11 @@
 #ifndef DSTD_ALLOCATOR_HXX
 #define DSTD_ALLOCATOR_HXX
 
+#include <new>
+
+#include "exception.hxx"
+
+
 namespace dstd
 {
 	template <class T>
@@ -14,28 +19,72 @@ namespace dstd
 			typedef const T* const_pointer;
 			typedef const T& const_reference;
 			
+			
 			allocator(){};
+			
+			
 			~allocator(){};
 			
-			T* address(T& obj) const;
-			const T* address(const T& obj) const;
+			
+			T* address(T& obj) const
+			{
+				// this breaks if T overloaded operator&
+				return &obj;
+			}
+			
+			
+			const T* address(const T& obj) const
+			{
+				// this breaks if T overloaded operator&
+				return &obj;
+			}
+
 			
 			/// Attempts to allocate a block of storage with a size large enough to contain n elements and returns a pointer to the first element.
 			/// Throws bad_alloc if it cannot allocate the total amount of storage requested.
-			T* allocate(unsigned int n);//, const T* hint = 0);
+			T* allocate(unsigned int n) //, const T* hint = 0)
+			{
+				T* a = 0;
+				
+				try
+				{
+					a = reinterpret_cast< T* >( ::operator new(n*sizeof(T)) );
+				}
+				catch( std::bad_alloc )
+				{
+					throw dstd::bad_alloc();
+				}
+				
+				if( a == 0 )
+				{
+					throw dstd::bad_alloc();
+				}
+				
+				return a;
+			}
+			
 			
 			/// Releases a block of storage previously allocated with member allocate and not yet released
 			/// The elements in the array are not destroyed by a call to this member function.
-			void deallocate(T* p, unsigned int n);
+			void deallocate(T* p, unsigned int n)
+			{
+				::operator delete( p );
+			}
+			
 			
 			/// Constructs an element object on the location pointed by p.
-			void construct(T* p, const T& value);
+			void construct(T* p, const T& value)
+			{
+				new(p) T(value);
+			}
+			
 			
 			/// Destroys in-place the object pointed by p.
-			void destroy(T* p);
+			void destroy(T* p)
+			{
+				p->~T();
+			}
 	};
 }
-
-#include "allocator.txx"
 
 #endif
