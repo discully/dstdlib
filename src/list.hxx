694b81048173d6b0a35440dde8ac1c59c2ee258c
Daniel Scully <daniel.i.scully@gmail.com>
2014-06-07 09:36:27 +0100

    add size_type and difference_type

--- src/list.hxx
+++ src/list.hxx
@@ -36,6 +36,8 @@ class dstd::list
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
+		typedef size_t size_type;
+		typedef ptrdiff_t difference_type;
 		class iterator;
 		class const_iterator;
 		typedef dstd::reverse_iterator<iterator> reverse_iterator;
