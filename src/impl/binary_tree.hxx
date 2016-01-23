7826196cb00fc65cc54f99a316b58c1520eddb0f
Daniel I Scully <discully@users.noreply.github.com>
2016-01-23 23:25:56 +0000

    formatting

--- src/impl/binary_tree.hxx
+++ src/impl/binary_tree.hxx
@@ -1,9 +1,7 @@
 #ifndef DSTD_IMPL_BINARY_TREE_HXX
 #define DSTD_IMPL_BINARY_TREE_HXX
 
-
-
-#include <cstddef>
+#include "../cstddef.hxx"
 
 
 
@@ -24,7 +22,6 @@ class dstd::impl::binary_tree
 		typedef size_t size_type;
 		
 		
-		
 		class node
 		{
 			public:
@@ -39,62 +36,62 @@ class dstd::impl::binary_tree
 				
 				node& operator= (const node& x)
 				{
-					this->up = x.up;
-					this->left = x.left;
-					this->right = x.right;
+					up = x.up;
+					left = x.left;
+					right = x.right;
 					return *this;
 				}
 				
 				void attach_child(node* child, bool left_child)
 				{
-					(left_child ? this->left : this->right) = child;
+					(left_child ? left : right) = child;
 					if( child != 0 ) child->up = this;
 				}
 				
 				bool is_header() const
 				{
 					// If the tree is empty, or has 1 node, then this check will be true:
-					if( this->left != 0 && this->right != 0 && this->left == this->right ) return true;
+					if( left != 0 && right != 0 && left == right ) return true;
 					
 					// If there is more than one node in the tree:
-					if( ( this->right != 0  && this->right->left == this ) && ( this->left != 0 && this->left->right == this ) ) return true;
+					if( ( right != 0  && right->left == this ) && ( left != 0 && left->right == this ) ) return true;
 					
 					return false;
 				}
 				
-				bool is_null() const { return this->is_header(); } // null means either 0 or the header, and only the latter can be checked
+				bool is_null() const { return is_header(); } // null means either 0 or the header, and only the latter can be checked
 				
-				node* left_child() { return this->left; }
-				const node* left_child() const { return this->left; }
+				node* left_child() { return left; }
+				const node* left_child() const { return left; }
 				
-				node* parent() { return this->up; }
-				const node* parent() const { return this->up; }
+				node* parent() { return up; }
+				const node* parent() const { return up; }
 				
-				node* right_child() { return this->right; }
-				const node* right_child() const { return this->right; }
+				node* right_child() { return right; }
+				const node* right_child() const { return right; }
 				
-				node* prev() { return const_cast<node*>( this->prev_node() ); }
-				const node* prev() const { return this->prev_node(); }
+				node* prev() { return const_cast<node*>( prev_node() ); }
+				const node* prev() const { return prev_node(); }
 				
-				node* next() { return const_cast<node*>( this->next_node() ); }
-				const node* next() const { return this->next_node(); }
+				node* next() { return const_cast<node*>( next_node() ); }
+				const node* next() const { return next_node(); }
 			
 			
 			private:
 				
 				const node* prev_node() const
 				{
-					if( this->is_header() )
+					if( is_header() )
 					{
 						// this is the header, so return the last node in the tree
-						return this->left;
+						return left;
 					}
-					else if( this->left != 0 ) // either this is not a leaf node, or it is the first node
+					else if( left != 0 ) // either this is not a leaf node, or it is the first node
 					{
-						if( this->left->is_header() ) return this->left; // this is the first node, so return the header
+						if( left->is_header() ) return left; // this is the first node, so return the header
 						
 						// find the right-most node in the left subtree
-						const node* p = this->left;
+						const node* p = left;
 						while( p->right != 0 )
 						{
 							p = p->right;
@@ -119,17 +116,17 @@ class dstd::impl::binary_tree
 				
 				const node* next_node() const
 				{
-					if( this->is_header() )
+					if( is_header() )
 					{
 						// this is the header, so return the first node in the tree
-						return this->right;
+						return right;
 					}
-					else if( this->right != 0 ) // either this is not a leaf node, or it is the last node
+					else if( right != 0 ) // either this is not a leaf node, or it is the last node
 					{
-						if( this->right->is_header() ) return this->right; // this is the last node, so return the header
+						if( right->is_header() ) return right; // this is the last node, so return the header
 						
 						// this has a right subtree, so find the left-most node in the right subtree
-						const node* p = this->right;
+						const node* p = right;
 						while( p->left != 0 )
 						{
 							p = p->left;
@@ -164,100 +161,100 @@ class dstd::impl::binary_tree
 		binary_tree()
 			: head( new node() ), n(0)
 		{
-			this->reset();
+			reset();
 		}
 		
 		
 		~binary_tree()
 		{
-			delete(this->head);
+			delete(head);
 		}
 		
 		
 		bool empty() const
 		{
-			return (this->size() == 0);
+			return (size() == 0);
 		}
 		
 		
-		node* header() { return this->head; }
-		const node* header() const { return this->head; }
+		node* header() { return head; }
+		const node* header() const { return head; }
 		
 		
 		void insert_left(node* new_node, node* parent)
 		{
-			if( this->is_null( parent ) )
+			if( is_null( parent ) )
 			{
-				this->set_root(new_node);
-				this->set_left_most(new_node);
-				this->set_right_most(new_node);
-				++(this->n);
+				set_root(new_node);
+				set_left_most(new_node);
+				set_right_most(new_node);
+				++(n);
 			}
-			else if( this->is_null( parent->left ) )			
+			else if( is_null( parent->left ) )			
 			{
 				parent->attach_child(new_node, true);
-				if( parent == this->left_most() ) this->set_left_most(new_node);
-				++(this->n);
+				if( parent == left_most() ) set_left_most(new_node);
+				++(n);
 			}
 		}
 		
 		
 		void insert_right(node* new_node, node* parent)
 		{
-			if( this->is_null( parent ) )
+			if( is_null( parent ) )
 			{
-				this->set_root(new_node);
-				this->set_left_most(new_node);
-				this->set_right_most(new_node);
-				++(this->n);
+				set_root(new_node);
+				set_left_most(new_node);
+				set_right_most(new_node);
+				++(n);
 			}
-			else if( this->is_null( parent->right ) )
+			else if( is_null( parent->right ) )
 			{
 				parent->attach_child(new_node, false);
-				if( parent == this->right_most() ) this->set_right_most(new_node);
-				++(this->n);
+				if( parent == right_most() ) set_right_most(new_node);
+				++(n);
 			}
 		}
 		
 		
 		bool is_null(const node* n) const
 		{
-			return( n == 0 || n == this->head );
+			return( n == 0 || n == head );
 		}
 		
 		
-		node* left_most() { return this->head->right; }
-		const node* left_most() const { return this->head->right; }
+		node* left_most() { return head->right; }
+		const node* left_most() const { return head->right; }
 		
 		
 		/// Removes a node from the tree.
 		/// Only nodes which do not have two children can be removed.
 		void remove(node* n)
 		{
-			if( this->is_null( n->left ) && this->is_null( n->right ) )
+			if( is_null( n->left ) && is_null( n->right ) )
 			{
 				// Removing a leaf-node (ie: one without any children)
 				
-				if( n == this->root() ) // removing the root node, leaving the tree empty
+				if( n == root() ) // removing the root node, leaving the tree empty
 				{
-					this->reset();
+					reset();
 				}
 				else // removing a regular leaf-node
 				{					
 					const bool left_child = (n == n->up->left);
 					
-					if( n == this->left_most() )
+					if( n == left_most() )
 					{
-						this->set_left_most( n->next() );
+						set_left_most( n->next() );
 					}
 					else if( left_child )
 					{
 						n->up->attach_child( 0, true );
 					}
 					
-					if( n == this->right_most() )
+					if( n == right_most() )
 					{
-						this->set_right_most( n->prev() );
+						set_right_most( n->prev() );
 					}
 					else if( ! left_child )
 					{
@@ -265,40 +262,40 @@ class dstd::impl::binary_tree
 					}
 				}
 				
-				--(this->n);
+				--(n);
 			}
-			else if( this->is_null(n->left) != this->is_null(n->right) ) // != behaves like xor
+			else if( is_null(n->left) != is_null(n->right) ) // != behaves like xor
 			{
 				// n has exactly one child
-				node* child = this->is_null(n->right) ? n->left : n->right;
+				node* child = is_null(n->right) ? n->left : n->right;
 				
-				if( n == this->left_most()  ) this->set_left_most(  n->next() );
-				if( n == this->right_most() ) this->set_right_most( n->prev() );
-				if( n == this->root() ) this->set_root( child );
+				if( n == left_most()  ) set_left_most(  n->next() );
+				if( n == right_most() ) set_right_most( n->prev() );
+				if( n == root() ) set_root( child );
 				
-				if( child != this->root() ) n->up->attach_child(child, (n->up->left == n));
+				if( child != root() ) n->up->attach_child(child, (n->up->left == n));
 				
-				--(this->n);
+				--(n);
 			}
 		}
 		
 		
-		node* right_most() { return this->head->left; }
-		const node* right_most() const { return this->head->left; }
+		node* right_most() { return head->left; }
+		const node* right_most() const { return head->left; }
 		
 		
-		node* root() { return this->head->up; }
-		const node* root() const { return this->head->up; }
+		node* root() { return head->up; }
+		const node* root() const { return head->up; }
 		
 		
-		size_type size() const { return this->n; }
+		size_type size() const { return n; }
 		
 		
 		void swap(node* n1, node* n2)
 		{
 			if( n1->up == n2 ) // n2 is n1's parent
 			{
-				this->swap(n2, n1); // try again, with the order reversed
+				swap(n2, n1); // try again, with the order reversed
 			}
 			else
 			{
@@ -307,7 +304,7 @@ class dstd::impl::binary_tree
 					const bool left_child = (n1->left == n2);
 					
 					// sort out the (non-header) things which point to n1 and n2	
-					if( n1 != this->root() )
+					if( n1 != root() )
 					{
 						if     ( n1->up->left  == n1 ) n1->up->left  = n2;
 						else if( n1->up->right == n1 ) n1->up->right = n2;
@@ -315,15 +312,15 @@ class dstd::impl::binary_tree
 					
 					if( left_child )
 					{
-						if( ! this->is_null(n1->right) ) n1->right->up = n2;
-						if( ! this->is_null(n2->right) ) n2->right->up = n1;
-						if( ! this->is_null(n2->left ) ) n2->left->up  = n1;
+						if( ! is_null(n1->right) ) n1->right->up = n2;
+						if( ! is_null(n2->right) ) n2->right->up = n1;
+						if( ! is_null(n2->left ) ) n2->left->up  = n1;
 					}
 					else
 					{
-						if( ! this->is_null(n1->left ) ) n1->left->up  = n2;
-						if( ! this->is_null(n2->left ) ) n2->left->up  = n1;
-						if( ! this->is_null(n2->right) ) n2->right->up = n1;
+						if( ! is_null(n1->left ) ) n1->left->up  = n2;
+						if( ! is_null(n2->left ) ) n2->left->up  = n1;
+						if( ! is_null(n2->right) ) n2->right->up = n1;
 					}
 					
 					// sort out n1 and n2
@@ -338,20 +335,20 @@ class dstd::impl::binary_tree
 				else // n1 and n2 are not connected
 				{
 					// sort out the (non-header) things which point to n1 and n2	
-					if( ! this->is_null(n1->up) )
+					if( ! is_null(n1->up) )
 					{
 						if     ( n1->up->left  == n1 ) n1->up->left  = n2;
 						else if( n1->up->right == n1 ) n1->up->right = n2;
 					}
-					if( ! this->is_null(n2->up) )
+					if( ! is_null(n2->up) )
 					{
 						if     ( n2->up->left  == n2 ) n2->up->left  = n1;
 						else if( n2->up->right == n2 ) n2->up->right = n1;
 					}
-					if( ! this->is_null(n1->left) )  n1->left->up  = n2;
-					if( ! this->is_null(n1->right) ) n1->right->up = n2;
-					if( ! this->is_null(n2->left) )  n2->left->up  = n1;
-					if( ! this->is_null(n2->right) ) n2->right->up = n1;
+					if( ! is_null(n1->left) )  n1->left->up  = n2;
+					if( ! is_null(n1->right) ) n1->right->up = n2;
+					if( ! is_null(n2->left) )  n2->left->up  = n1;
+					if( ! is_null(n2->right) ) n2->right->up = n1;
 					
 					// sort out n1 and n2
 					node temp(*n1);
@@ -361,14 +358,14 @@ class dstd::impl::binary_tree
 				
 				// sort out the header things which point to n1 or n2
 				
-				if     ( this->head->up == n1 ) this->head->up = n2;
-				else if( this->head->up == n2 ) this->head->up = n1;
+				if     ( head->up == n1 ) head->up = n2;
+				else if( head->up == n2 ) head->up = n1;
 				
-				if     ( this->head->left == n1 ) this->head->left = n2;
-				else if( this->head->left == n2 ) this->head->left = n1;
+				if     ( head->left == n1 ) head->left = n2;
+				else if( head->left == n2 ) head->left = n1;
 				
-				if     ( this->head->right == n1 ) this->head->right = n2;
-				else if( this->head->right == n2 ) this->head->right = n1;
+				if     ( head->right == n1 ) head->right = n2;
+				else if( head->right == n2 ) head->right = n1;
 			}
 		}
 	
@@ -377,27 +374,27 @@ class dstd::impl::binary_tree
 		
 		void reset()
 		{
-			this->head->up = this->head;
-			this->head->left = this->head;
-			this->head->right = this->head;
+			head->up = head;
+			head->left = head;
+			head->right = head;
 		}
 		
 		void set_left_most(node* n)
 		{
-			this->head->right = n;
-			n->left = this->head;
+			head->right = n;
+			n->left = head;
 		}
 		
 		void set_right_most(node* n)
 		{
-			this->head->left = n;
-			n->right = this->head;
+			head->left = n;
+			n->right = head;
 		}
 		
 		void set_root(node* n)
 		{
-			this->head->up = n;
-			n->up = this->head;
+			head->up = n;
+			n->up = head;
 		}
 		
 		node* head;
